9f2b4752190a7e1292395bf60de054aa2534e999
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-05-01 16:27:45 -0400

    whateva

--- lambdas/lambda_vote/lambda_vote.py
+++ lambdas/lambda_vote/lambda_vote.py
@@ -1,8 +1,13 @@
 import boto3
 from StockList import stocks
 
-def lambda_vote(comments: list):
+dynamodb = boto3.resource('dynamodb', region_name='us-east-3')
+table = dynamodb.Table('daily_bets_table')
+
+def lambda_vote(event, context):
+    comments = event['comments']
     
+    for comment in comments:
 
     # send to db
 
