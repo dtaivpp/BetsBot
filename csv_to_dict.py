f761adddf0d4a5425217fff09f4b2bfc8b98b66b
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-05-01 16:11:04 -0400

    created valid dict in file

--- csv_to_dict.py
+++ csv_to_dict.py
@@ -11,8 +11,10 @@ symbol_dict = { row['Symbol'] : row['Description'] for index, row in df.iterrows
 
 # Write that dict to file
 with open(join('.','resources','StockList.py'), '+w') as f:
-    f.write("stocks = ") 
-    f.write(str(symbol_dict))
+    f.write("stocks = {\n") 
+    for key, value  in symbol_dict.items():
+        f.write(f"\t\"{key}\": \"{value}\",\n")
+    f.write("\t}")
 
 
 source = join('.','resources','StockList.py')
@@ -20,7 +22,11 @@ dest = join('.','lambdas')
 
 # Copy the StockList file to the lambda folders
 #    Excluding the root folder
-for x in walk(dest)[1:]:
+for x in walk(dest):
+    
+    if x[0] == dest:
+        continue
+
     # Destination file name
     tmp_dest = join(x[0], 'StockList.py')
 
