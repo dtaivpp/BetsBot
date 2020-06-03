037ea3ec946dbd2cc26b1aa77695d282585be231
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2020-06-03 17:00:51 -0400

    further work on reporting results

--- lambdas/lambda_daily_price_fetcher/lambda_daily_price_fetcher.py
+++ lambdas/lambda_daily_price_fetcher/lambda_daily_price_fetcher.py
@@ -8,6 +8,7 @@ tiingo_token = environ['tiingo_token']
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-3')
 symbol_outlook_table = dynamodb.Table('Symbol_Outlook')
+symbol_price_table = dynamodb.Table('Symbol_Price')
 
 headers = {
         'Content-Type': 'application/json',
@@ -20,6 +21,10 @@ requestResponse = requests.get("https://api.tiingo.com/api/test/",
 def lambda_daily_price_fetcher(event, context):
     ticker_list = get_daily_tickers()
 
+    price_list = get_stock_prices(ticker_list)
+
+    store_prices(price_list)
+
     pass
 
 def get_daily_tickers():
@@ -29,13 +34,47 @@ def get_daily_tickers():
         )
     symbols = []
     for item in response['Items']:
-        for key in item.keys()
+        for key in item.keys():
             symbols.append(key)
 
     return symbols
 
 
 # get stock prices for those tickers
+def get_stock_prices(symbols: list) -> list:
+    price_list=[]
+
+    for symbol in symbols:
+        url = generate_url(symbol)
+        response = requests.get(url=url, headers=headers)
+        price = response.json()
+        price['symbol'] = symbol
+        price_list.append(price)
+
+
+def generate_url(symbol: str) -> str:
+    return f"https://api.tiingo.com/tiingo/daily/{symbol}/prices"
+
+
+def store_prices(prices: list):
+    with symbol_outlook_table.batch_write() as batch:
+        for price in prices:
+            batch.put_item(
+                Item=tiingo_serialize(price)
+            )
+
+
+def tiingo_serialize(item):
+    return {
+        'symbol': item['symbol'],
+        'date': current_day_utc(),
+        'open': item['open'],
+        'close': item['close'],
+        'high': item['high'],
+        'low': item['low'],
+        'volume': item['volume']
+    }
+
 
 def current_day_utc() -> int:
     """
